c690cf994ca3767d41f56cb63c705d6a6d450fd5
Ismael Carrillo <iscarrillo65@gmail.com>
2023-08-16 19:53:31 -0300

    commit 1

---
+++ TemaA.py
@@ -0,0 +1,63 @@
+#Ejercicios complementarios
+print("Ejercicio 1")
+numero1 = 7
+print(numero1)
+print("Ejercicio 2")
+numero2 = 2.6
+print(numero2)
+print("Ejercicio 3")
+suma = numero1 + numero2
+print(suma)
+print("Ejercicio 4")
+resta = numero1 - numero2
+print(resta)
+multiplicacion = numero1*numero2
+print(multiplicacion)
+division = numero1/numero2
+print(division)
+
+print("Ejercicio 5")
+nombre = "Ismael"
+print(nombre)
+print("Ejercicio 6")
+precio = 80.5
+print(precio)
+print("Ejercicio 7")
+descuento = 0.25
+print(descuento)
+print("Ejercicio 8")
+precio_final = precio-(descuento*precio)
+print(precio_final)
+
+print("Ejercicio 9")
+cadena = "Árboles verdes en Mendoza"
+print(cadena)
+print("Ejercicio 10")
+longitud = len(cadena)
+print(longitud) 
+print("Ejercicio 11")
+precio2 = 1.2
+precio2 = int(precio2)
+print(precio2)
+print("Ejercicio 12")
+nombre = "Sergio"
+apellido = "Busquet"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+print("Ejercicio 13")
+edad = 20
+edad += 5
+edad -= 10
+print(edad)
+print("Ejercicio 14")
+altura = 1.66
+altura *= 5
+altura /= 3
+print(altura)
+print("Ejercicio 15")
+mi_nombre = "ISMAEL CARRILLO"
+mi_nombre2 = mi_nombre.lower()
+print(mi_nombre2)
+print("Ejercicio 16")
+mi_nombre3 = (mi_nombre.lower()).title()
+print(mi_nombre3)
